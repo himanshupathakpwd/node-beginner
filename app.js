48fd1ddeabbcb9cfe98ce9d03c48ebe6f41cded1
Himanshu Pathak <himanshupathakpwd@gmail.com>
2015-09-19 18:37:26 +0530

    added end() function to stop

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ var http = require('http');
 var myServer = http.createServer(function (request, response) {
     response.writeHead('200', {"Content-Type" : "text/plain"});
     response.write('Hello World');
+    response.end();
 });
 myServer.listen(3000);
